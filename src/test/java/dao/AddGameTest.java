dcce09be4d1b602ea529f63e7239184e1f60bc25
orIsc <orjslash27@gmail.com>
2021-07-15 23:26:59 +0300

    added chat_room and messages classes and add them to the response and request create queries and dao

--- src/test/java/dao/AddGameTest.java
+++ src/test/java/dao/AddGameTest.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 
 public class AddGameTest {
     private Game game;
+    private Game game2;
+    private Game game3;
+    private Game game4;
+    private Game game5;
     private ArrayList<Game> gamesList;
     private DbHandle dbHandle;
 
@@ -17,13 +21,21 @@ public class AddGameTest {
     public void beforeTest() {
         dbHandle = DbHandleImpl.getInstance();
         gamesList = new ArrayList<>();
-        //game = new Game(0, "Warcraft", "/Users/orissacci/Downloads/warcraft.png", "strategy");
+        game = new Game("Warcraft", 2131165334, "strategy");
+        game2 = new Game("Gta v", 2131165286, "action");
+        game3 = new Game("fifa22", 2131165285, "sport");
+        game4 = new Game("nba 2k", 2131165317, "sport");
+        game5 = new Game("heroes", 2131165287, "strategy");
         
     }
 
     @Test
     public void test() {
         dbHandle.addGame(game);
+        dbHandle.addGame(game2);
+        dbHandle.addGame(game3);
+        dbHandle.addGame(game4);
+        dbHandle.addGame(game5);
     }
 
     @After
